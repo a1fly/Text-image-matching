5ac2a477f261b871a44eea6499420e24273d5615
a1fly <zongcunyao2004@163.com>
2024-12-03 15:34:13 +0800

    新增后端图片文件的地址转移

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,14 +2,19 @@
 module.exports = {
   devServer: {
     webSocketServer:false,
-    port: 8081,          // 设置端口
+    port: 8082,          // 设置端口
     host: '0.0.0.0',     // 允许外部访问，默认是 'localhost'
     https: false,        // 是否启用 https
     proxy: {             // 配置代理，解决跨域问题
       '/api': {
-        target: 'http://localhost:5000',  // 后端 API 地址
+        target: 'http://localhost:5001',  // 后端 API 地址
         changeOrigin: true,               // 是否改变请求源
         pathRewrite: { '^/': '' }      // 重写路径
+      },
+      '/static/uploads': {
+        target: 'http://localhost:5001',  // 后端静态文件地址
+        changeOrigin: true,               // 是否改变请求源
+        pathRewrite: { '^/static/uploads': '/static/uploads' }  // 重写路径
       }
     }
   },
